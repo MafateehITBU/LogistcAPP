27b24420f7edf7aed0c0d05a1466577a0ff44359
Sarasuleiman02 <sarasuleiman1234@gmail.com>
2025-01-19 10:44:17 +0300

    update project

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,34 @@
+const asyncHandler = require('express-async-handler');
+const Admin = require('../models/Admin');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Sign up
+const register = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Email and Password are required');
+    }
+    // Checking if the user already in the database
+    const userAvailable = await Admin.findOne({ email });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await Admin.create({
+        email,
+        password: hashedPassword
+    });
+
+    if (user) {
+        res.status(200).json({ _id: user.id, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error('User data is not valid');
+    }
+    res.json({ message: "Register User" });
+});
